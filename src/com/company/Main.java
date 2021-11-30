f73bfc9c1070b6260f94dbb7e0fd205fbe048279
Erick Rangel <erick1rg@gmail.com>
2021-11-29 22:09:32 -0500

    Design Pattern FactoryMethod Design Pattern Abstract Factory Design Pattern Builder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import com.company.creational.factorymethod.Payment;
+import com.company.creational.factorymethod.PaymentFactory;
+import com.company.creational.factorymethod.TypePayment;
+import com.company.creational.abstractfactory.AbstractFactory;
+import com.company.creational.abstractfactory.Card;
+import com.company.creational.abstractfactory.FactoryProvider;
+import com.company.creational.abstractfactory.PaymentMethod;
+
 public class Main {
 
     public static void main(String[] args) {
+        testAbstractFactory();
+    }
 
+    private static void testAbstractFactory() {
+        AbstractFactory abstractFactory = FactoryProvider.getFactory("Card");
+        Card card = (Card) abstractFactory.create("VISA");
+
+        AbstractFactory abstractFactory1 = FactoryProvider.getFactory("PaymentMethod");
+        PaymentMethod paymentMethod = (PaymentMethod) abstractFactory1.create("CREDIT");
+
+        System.out.println("Un tarjeta de tipo = " + card.getCardType() + " con el método de pago: " + paymentMethod.doPayment());
+    }
+
+    //Probar método testFactoryMethod();
+    private static void testFactoryMethod(){
+        Payment payment = PaymentFactory.buildPayment(TypePayment.CARD);
+        payment.doPayment();
     }
 }
--- src/com/company/creational/factorymethod/PaymentFactory.java
+++ src/com/company/creational/factorymethod/PaymentFactory.java
@@ -1,4 +1,15 @@
 package com.company.creational.factorymethod;
 
 public class PaymentFactory {
+
+    public static Payment buildPayment(TypePayment typePayment){
+        switch (typePayment){
+            case GOOGLEPAYMENT:
+                return new GooglePayment();
+            case CARD:
+                return new CardPayment();
+            default:
+                return new CardPayment();
+        }
+    }
 }
